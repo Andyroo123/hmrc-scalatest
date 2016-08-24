d1f2a5c6df02e66f6dc7c31b95ee95432c356a46
Andyroo123 <andy@andyroo.info>
2016-08-24 17:36:59 +0100

    Added missed off scenarios for checkout. Added discount functionality for BOGOF apples and 3for2 oranges

--- src/main/scala/net/hmrc/scalatest/Checkout.scala
+++ src/main/scala/net/hmrc/scalatest/Checkout.scala
@@ -5,26 +5,51 @@ package net.hmrc.scalatest
   */
 object Checkout extends App {
 
+  final val APPLE = "Apple"
+  final val ORANGE = "Orange"
+
+  final val APPLE_PRICE = 60
+  final val ORANGE_PRICE = 25
+
+  final val APPLE_DISCOUNT_TRIGGER = 2
+  final val ORANGE_DISCOUNT_TRIGGER = 3
+
+  final val APPLE_DISCOUNT_AMOUNT = APPLE_PRICE
+  final val ORANGE_DISCOUNT_AMOUNT = ORANGE_PRICE
+
   def checkout(items: List[String]): String = {
     val total: Int = addUpItems(items)
-    convertToPounds(total)
+    val discount: Int = calculateDiscount(items)
+    val grandTotal = total - discount
+    convertPenceToPounds(grandTotal)
   }
 
   def addUpItems(items: List[String]): Int = {
 
     items match {
-      case "Apple" :: remainingItems =>
-        60 + addUpItems(remainingItems)
-      case "Orange" :: remainingItems =>
-        25 + addUpItems(remainingItems)
+      case `APPLE` :: remainingItems =>
+        APPLE_PRICE + addUpItems(remainingItems)
+      case `ORANGE` :: remainingItems =>
+        ORANGE_PRICE + addUpItems(remainingItems)
       case Nil =>
         0
+      case _ =>
+        println(s"Unexpected item has been scanned ${items.head}")
+        throw new Exception(s"Unexpected item has been scanned: ${items.head}")
     }
   }
 
-  def convertToPounds(pence: Int): String = {
+  def convertPenceToPounds(pence: Int): String = {
     val pounds: Double = pence.toDouble / 100
     f"£$pounds%1.2f"
+  }
+
+  def calculateDiscount(items: List[String]): Int = {
+    val appleCount = items.count(_ equals APPLE)
+    val discountedApples = appleCount / APPLE_DISCOUNT_TRIGGER
+    val orangeCount = items.count(_ equals ORANGE)
+    val discountedOranges = orangeCount / ORANGE_DISCOUNT_TRIGGER
+    (discountedApples * APPLE_DISCOUNT_AMOUNT) + (discountedOranges * ORANGE_DISCOUNT_AMOUNT)
   }
 
 }
--- src/test/scala/net/hmrc/scalatest/CheckoutSpec.scala
+++ src/test/scala/net/hmrc/scalatest/CheckoutSpec.scala
@@ -23,6 +23,11 @@ class CheckoutSpec extends mutable.Specification {
       Checkout.addUpItems(items) mustEqual 120
     }
 
+    "Charge 6 apples at 360p" in {
+      val items: List[String] = List.fill(6)("Apple")
+      Checkout.addUpItems(items) mustEqual 360
+    }
+
     "Charge 1 orange at 25p" in {
       val items: List[String] = List("Orange")
       Checkout.addUpItems(items) mustEqual 25
@@ -33,9 +38,28 @@ class CheckoutSpec extends mutable.Specification {
       Checkout.addUpItems(items) mustEqual 25
     }
 
+    "Charge 6 oranges at 150p" in {
+      val items: List[String] = List.fill(6)("Orange")
+      Checkout.addUpItems(items) mustEqual 150
+    }
+
     "Charge 1 apple and 1 orange at 85p" in {
       val items: List[String] = List("Orange", "Apple")
       Checkout.addUpItems(items) mustEqual 85
+    }
+
+    "Charge 4 apple and 14 orange at 85p" in {
+      val apples: List[String] = List.fill(4)("Apple")
+      val oranges: List[String] = List.fill(14)("Orange")
+      val items: List[String] = apples ++ oranges
+      Checkout.addUpItems(items) mustEqual 590
+    }
+
+    "Charge 24 apple and 8 orange at 85p" in {
+      val apples: List[String] = List.fill(24)("Apple")
+      val oranges: List[String] = List.fill(8)("Orange")
+      val items: List[String] = apples ++ oranges
+      Checkout.addUpItems(items) mustEqual 1640
     }
 
     "Charge 601 apples and 24 oranges at 36660p" in {
@@ -45,42 +69,47 @@ class CheckoutSpec extends mutable.Specification {
       Checkout.addUpItems(items) mustEqual 36660
     }
 
+    "Unexpected item throws error" in {
+      val items: List[String] = List("Apple", "Orange", "Banana")
+      Checkout.addUpItems(items) must throwA[Exception].like { case e => e.getMessage mustEqual "Unexpected item has been scanned: Banana"}
+    }
+
   }
 
   "convertToPounds" should {
     "Convert 0p to £0.00" in {
       val pence: Int = 0
-      Checkout.convertToPounds(pence) mustEqual "£0.00"
+      Checkout.convertPenceToPounds(pence) mustEqual "£0.00"
     }
 
     "Convert 25p to £0.25" in {
       val pence: Int = 25
-      Checkout.convertToPounds(pence) mustEqual "£0.25"
+      Checkout.convertPenceToPounds(pence) mustEqual "£0.25"
     }
 
     "Convert 60p to £0.60" in {
       val pence: Int = 60
-      Checkout.convertToPounds(pence) mustEqual "£0.60"
+      Checkout.convertPenceToPounds(pence) mustEqual "£0.60"
     }
 
     "Convert 100p to £1.00" in {
       val pence: Int = 100
-      Checkout.convertToPounds(pence) mustEqual "£1.00"
+      Checkout.convertPenceToPounds(pence) mustEqual "£1.00"
     }
 
     "Convert 111p to £1.11" in {
       val pence: Int = 111
-      Checkout.convertToPounds(pence) mustEqual "£1.11"
+      Checkout.convertPenceToPounds(pence) mustEqual "£1.11"
     }
 
     "Convert 201p to £2.01" in {
       val pence: Int = 201
-      Checkout.convertToPounds(pence) mustEqual "£2.01"
+      Checkout.convertPenceToPounds(pence) mustEqual "£2.01"
     }
 
     "Convert 31444p to £314.44" in {
       val pence: Int = 31444
-      Checkout.convertToPounds(pence) mustEqual "£314.44"
+      Checkout.convertPenceToPounds(pence) mustEqual "£314.44"
     }
   }
 
@@ -90,6 +119,54 @@ class CheckoutSpec extends mutable.Specification {
       val items: List[String] = List("Apple")
       Checkout.checkout(items) mustEqual "£0.60"
     }
+
+    "Charge 1 orange with a readable output" in {
+      val items: List[String] = List("Orange")
+      Checkout.checkout(items) mustEqual "£0.25"
+    }
+
+    "Charge 5 apples and 1 orange with a readable output" in {
+      val apples: List[String] = List.fill(5)("Apple")
+      val oranges: List[String] = List("Orange")
+      val items = apples ++ oranges
+      Checkout.checkout(items) mustEqual "£2.05"
+    }
+  }
+
+  "Calculate discount" should {
+    "Be 0p for 1 apple" in {
+      val items: List[String] = List("Apple")
+      Checkout.calculateDiscount(items) mustEqual 0
+    }
+
+    "Be 60p for 2 apples" in {
+      val items: List[String] = List("Apple")
+      Checkout.calculateDiscount(items) mustEqual 0
+    }
+
+    "Be 60p for 3 apples" in {
+      val items: List[String] = List("Apple")
+      Checkout.calculateDiscount(items) mustEqual 0
+    }
+
+    "Be 0p for 1 orange" in {
+      val items: List[String] = List.fill(1)("Orange")
+      Checkout.calculateDiscount(items) mustEqual 0
+    }
+
+    "Be 0p for 2 oranges" in {
+      val items: List[String] = List.fill(2)("Orange")
+      Checkout.calculateDiscount(items) mustEqual 0
+    }
+
+    "Be 1070p for 44 oranges and 25 apples" in {
+      val oranges: List[String] = List.fill(44)("Orange") // costs 1100p discount 350p
+      val apples: List[String] = List.fill(25)("Apple") // costs 1500p discount 720p
+      val items: List[String] = oranges ++ apples // total costs 2600p with discount of 1070p
+      Checkout.calculateDiscount(items) mustEqual 1070
+    }
+
+
   }
 
 }
